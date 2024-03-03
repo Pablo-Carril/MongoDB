8360fe0488edf47cb4cba9f28f3cbe0b41385ae5
Pablo Carril <pablogcarril@gmail.com>
2024-03-03 09:07:53 -0300

    Uri en variable de entorno

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,9 +1,12 @@
 
-import mongoose from "mongoose";  
+import mongoose from "mongoose";
+//import dotenv from dotenv 
+const URI = process.env.MONGODB_URI;
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 //const URI = 'mongodb://localhost:27017/Equipos'  //pongo la URi + /Equipos que es la base de datos. por default usa la base test. por eso no da error.
 //La URI la obtenemos desde Conection String de ATLAS, o en mongosh db.getMongo().
-const URI = 'mongodb+srv://pablogcarril:F3l1p31881@cluster0.jqnbmlg.mongodb.net/Equipos'
+//const URI = 'mongodb+srv://pablogcarril:F3l1p31881@cluster0.jqnbmlg.mongodb.net/Equipos'
 
 export const initdb = async ()=> {
   try {
