fa315d86ec5714c85ccd928e2654e033b08a6cb9
Pablo Carril <pablogcarril@gmail.com>
2024-03-21 18:53:21 -0300

    agrego nueva función mostrarpopup

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import indexRouter from './routers/index.router.js'
 import userRouter from './routers/users.router.js'
 import validadorRouter from './routers/Validador.router.js'
-//import handlebarsHelp from 'handlebars';  //es es el handlebars básico. si usamos expres no usarlo
+//import handlebarsHelp from 'handlebars';  //es es el handlebars básico. si usamos expres-handlebars no hace falta este.
 import handlebars from 'express-handlebars'  //es es el extendido para integración con express
 
 const helpers = handlebars.create()
@@ -14,10 +14,10 @@ helpers.handlebars.registerHelper('isSelected', function(value, expectedValue) {
 import {__dirname} from './utils.js'
 import path from 'path'
 
-//import { log } from 'console'
-import morgan from 'morgan'
+//import { log } from 'console'  
+import morgan from 'morgan'    //morgan permite ver las solicitudes http por consola
 
-//const PUERTO = 3040   ***** AHORA LO VAMOS A MANEJAR DESDE SERVER.JS *********
+//const PUERTO = 3040   ***** AHORA LO VAMOS A MANEJAR DESDE .ENV *********
 
 const app = express()    //necesitamos ejecutarlo para que nos devuelva el objeto con todas sus propiedades y métodos
 
@@ -45,24 +45,24 @@ app.set('view engine', 'handlebars')              //establecemos la extensión. 
 //  next()          //pasamos al siguiente middleware
 //})
 
-app.use(express.json())   //permite usar JSON en el body de los req Http
-app.use(express.urlencoded({extended: true}))   //para que hacepte url extendidas, con símbolos &, :, #, etc
-//app.use(express.static('public'))      //define una carpeta public con contenido de archivos estáticos
+app.use(express.json())   //permite usar JSON en el body de los req Http. si necesitamos texto podemos usar express.text
+app.use(express.urlencoded({extended: true}))   //para que hacepte datos de FORMULARIOS y url extendidas, o sea símbolos &, :, #, etc
+// también los datos de los formularios los convierte a formato Json. si no no se podrían leer.
 app.use(express.static(path.join(__dirname, './public')))  //definimos la carpeta estática. usamos path para definir mejor una carpeta absoluta
-//Ahora /public es la carpeta raíz de todo el proyecto y no se podrá acceder a ninguna carpeta superior. los atajos para encontrar rutas en VSCode ya no sirven.
+//Ahora /public es la carpeta raíz de todo el proyecto y no se podrá acceder a ninguna carpeta superior. los atajos para encontrar rutas en VSCode ya no sirven del lado del CLIENTE.
 //por DEFECTO el server envía el INDEX.HTML ubicado dentro de public. no es necesario especificarlo. sacar index.html para que funcione handlebars.
 
 app.use('/', indexRouter)    //router del raíz. aquí especificamos el de handlebars, pero si existe index.html en public toma ese primero.
 app.use('/api/users', userRouter)     //agregamos todos los routers dentro de /api mediante comas (userRouter, carritoRouter, etc)
 app.use('/api/validadores', validadorRouter)
 
-app.use( (req,res) => {
+app.use( (req,res) => {        //middleware para cualquier otra ruta que no tenga router
   res.send('No se encontró la página')
   res.status(404)
 })
 
-app.use( (error, req, res, next) => {
-  const mensaje = 'ha ocurrido un error desconocido'
+app.use( (error, req, res, next) => {     //nuestro propio middleware de error cuando todos los anteriores fallan.
+  const mensaje = 'ha ocurrido un error desconocido: '
   console.log(mensaje, error)
   res.status(500).json({mensaje})
 })
